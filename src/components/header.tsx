0ffc105258d9ddd6ea68614777c59771350ebe95
puhakkn2 <noora.puhakka@aalto.fi>
2024-05-11 21:17:28 +0300

    fix: Better styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
 import {
   AppBar,
-  Button,
   Container,
   IconButton,
   Menu,
@@ -12,7 +11,7 @@ import theme from "../styles/theme";
 import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
 import { useState } from "react";
 import "../styles/Menu.css";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
@@ -25,16 +24,34 @@ export default function Header() {
     <AppBar
       position="sticky"
       color="transparent"
-      sx={{ width: "100vw", height: "5vh", boxShadow: "none" }}
+      sx={{
+        width: "100vw",
+        height: "5vh",
+        boxShadow: "none",
+      }}
     >
-      <Toolbar sx={{ height: "5vh", minHeight: "0px" }}>
-        <Typography fontFamily="inherit" fontSize={12}>
-          Noora Puhakka
-        </Typography>
+      <Toolbar
+        sx={{
+          height: "5vh",
+          minHeight: "0px",
+          display: "flex",
+        }}
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography fontFamily="inherit" fontSize={12}>
+            Noora Puhakka
+          </Typography>
 
-        <IconButton>
-          <MenuIcon htmlColor="#f7f0d7" onClick={handleMenu}></MenuIcon>
-        </IconButton>
+          <IconButton onClick={handleMenu}>
+            <MenuIcon htmlColor="#f7f0d7"></MenuIcon>
+          </IconButton>
+        </Container>
 
         <Menu
           id="menu"
@@ -45,19 +62,25 @@ export default function Header() {
           sx={{
             top: "10px",
             left: "42vw",
-            height: "100vh",
-            // backgroundColor: "#f7f0d7",
+            height: "95vh",
+            bgColor: "#f7f0d7",
             maxHeight: "none",
           }}
         >
-          <MenuItem onClick={handleMenu}>
+          <MenuItem
+            onClick={handleMenu}
+            sx={{ display: "flex", justifyContent: "right" }}
+          >
             <CloseIcon fontSize="small" sx={{ position: "left" }} />
           </MenuItem>
-          <MenuItem onClick={handleMenu} sx={{ textTransform: "none" }}>
-            <Typography fontFamily={theme.text?.primary?.font} fontSize={12}>
-              Me?
-            </Typography>
-          </MenuItem>
+
+          <Link to={"/"}>
+            <MenuItem onClick={handleMenu} sx={{ textTransform: "none" }}>
+              <Typography fontFamily={theme.text?.primary?.font} fontSize={12}>
+                Me?
+              </Typography>
+            </MenuItem>
+          </Link>
 
           <Link to={"/projects"}>
             <MenuItem onClick={handleMenu} sx={{ textTransform: "none" }}>
@@ -66,11 +89,14 @@ export default function Header() {
               </Typography>
             </MenuItem>
           </Link>
-          <MenuItem onClick={handleMenu} sx={{ textTransform: "none" }}>
-            <Typography fontFamily={theme.text?.primary?.font} fontSize={12}>
-              Experience?
-            </Typography>
-          </MenuItem>
+
+          <Link to="/projects">
+            <MenuItem onClick={handleMenu} sx={{ textTransform: "none" }}>
+              <Typography fontFamily={theme.text?.primary?.font} fontSize={12}>
+                Experience?
+              </Typography>
+            </MenuItem>
+          </Link>
         </Menu>
       </Toolbar>
     </AppBar>
